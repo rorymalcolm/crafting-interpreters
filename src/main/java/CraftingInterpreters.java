43fd3f926967451c48a7d5b93440cd3f6c186132
Rory Malcolm <rorymckenziemalcolm@gmail.com>
2022-04-22 23:06:07 +0100

    init

---
+++ src/main/java/CraftingInterpreters.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Objects;
+
+import static java.nio.charset.Charset.defaultCharset;
+
+public class CraftingInterpreters {
+
+    private static boolean hadError = false;
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("Hello my name is Rory");
+        if (args.length > 1) {
+            System.out.println("Usage: jlox [script]");
+            System.exit(64);
+        } else if (args.length == 1) {
+            runFile(args[0]);
+        } else {
+            runPrompt();
+        }
+    }
+
+    private static void runFile(String filePath) throws IOException {
+        byte[] bytes = Files.readAllBytes(Paths.get(filePath));
+        run(new String(bytes, defaultCharset()));
+    }
+
+    private static void runPrompt() throws IOException {
+        var isr = new InputStreamReader(System.in);
+        var bufferedReader = new BufferedReader(isr);
+        for(;;) {
+            System.out.print("> ");
+            var line = bufferedReader.readLine();
+            if (Objects.equals(line, "exit")) {
+                break;
+            }
+            run(line);
+            hadError = false;
+        }
+    }
+
+    private static void run(String source) {
+        var scanner = new TokenScanner(source);
+        var scannedTokens = scanner.scanTokens();
+        if (hadError) {
+            System.exit(65);
+        }
+    }
+}
---
+++ src/main/java/TokenScanner.java
@@ -0,0 +1,80 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class TokenScanner {
+    private int start = 0;
+    private int current = 0;
+    private int line = 1;
+
+    List<Token> tokens;
+    final String source;
+
+    public TokenScanner(String source) {
+        tokens = new ArrayList<>();
+        this.source = source;
+    }
+
+    public List<Token> scanTokens() {
+        while (!isAtEnd()) {
+            start = current;
+            scanToken();
+        }
+        return tokens;
+    }
+
+    private void scanToken() {
+        char c = advance();
+        switch (c) {
+            case '(':
+                addToken(TokenType.LEFT_PAREN);
+                break;
+            case ')':
+                addToken(TokenType.RIGHT_PAREN);
+                break;
+            case '{':
+                addToken(TokenType.LEFT_BRACE);
+                break;
+            case '}':
+                addToken(TokenType.RIGHT_BRACE);
+                break;
+            case ',':
+                addToken(TokenType.COMMA);
+                break;
+            case '.':
+                addToken(TokenType.DOT);
+                break;
+            case '-':
+                addToken(TokenType.MINUS);
+                break;
+            case '+':
+                addToken(TokenType.PLUS);
+                break;
+            case ';':
+                addToken(TokenType.SEMICOLON);
+                break;
+            case '*':
+                addToken(TokenType.STAR);
+                break;
+            default:
+                break;
+        }
+    }
+
+    public boolean isAtEnd() {
+        return current >= source.length();
+    }
+
+    private char advance() {
+        current++;
+        return source.charAt(current - 1);
+    }
+
+    private void addToken(TokenType type) {
+        addToken(type, null);
+    }
+
+    private void addToken(TokenType type, Object literal) {
+        String text = source.substring(start, current);
+        tokens.add(new Token(type, text, literal, line));
+    }
+}
