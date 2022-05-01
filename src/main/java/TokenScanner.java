056f8a14f68a6007aa3032ac5d7515edfaf559c7
Rory Malcolm <rorymckenziemalcolm@gmail.com>
2022-05-01 07:40:12 +0100

    begin keyword mapping

--- src/main/java/TokenScanner.java
+++ src/main/java/TokenScanner.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class TokenScanner {
     private int start = 0;
@@ -8,6 +10,28 @@ public class TokenScanner {
 
     List<Token> tokens;
     final String source;
+
+    private static final Map<String, TokenType> keywords;
+
+    static {
+        keywords = new HashMap<>();
+        keywords.put("and", TokenType.AND);
+        keywords.put("class", TokenType.CLASS);
+        keywords.put("else", TokenType.ELSE);
+        keywords.put("false", TokenType.FALSE);
+        keywords.put("for", TokenType.FOR);
+        keywords.put("fun", TokenType.FUN);
+        keywords.put("if", TokenType.IF);
+        keywords.put("nil", TokenType.NIL);
+        keywords.put("or", TokenType.OR);
+        keywords.put("print", TokenType.PRINT);
+        keywords.put("return", TokenType.RETURN);
+        keywords.put("super", TokenType.SUPER);
+        keywords.put("this", TokenType.THIS);
+        keywords.put("true", TokenType.TRUE);
+        keywords.put("var", TokenType.VAR);
+        keywords.put("while", TokenType.WHILE);
+    }
 
     public TokenScanner(String source) {
         tokens = new ArrayList<>();
@@ -68,7 +92,7 @@ public class TokenScanner {
                 addToken(match('=') ? TokenType.GREAT_EQUAL : TokenType.GREATER);
                 break;
             case '/':
-                if(match('/')) {
+                if (match('/')) {
                     while (peek() != '\n' && !isAtEnd()) {
                         advance();
                     }
@@ -83,8 +107,17 @@ public class TokenScanner {
             case '\n':
                 line++;
                 break;
+            //handling of literals
+            case '"':
+                stringLiteral();
+                break;
             default:
-                CraftingInterpreters.error(line, "Encountered an error during interpretation");
+                if (isDigit(c)) {
+                    numberLiteral();
+                    break;
+                } else {
+                    CraftingInterpreters.error(line, "Encountered an error during interpretation");
+                }
                 break;
         }
     }
@@ -114,14 +147,68 @@ public class TokenScanner {
         return source.charAt(current);
     }
 
-    private boolean match(char expected) {
-        if(isAtEnd()) {
-           return false;
+    private char peekNext() {
+        if (current + 1 > source.length()) {
+            return '\0';
         }
-        if(source.charAt(current) != expected) {
+        return source.charAt(current + 1);
+    }
+
+    private boolean match(char expected) {
+        if (isAtEnd()) {
+            return false;
+        }
+        if (source.charAt(current) != expected) {
             return false;
         }
         current++;
         return true;
+    }
+
+    private void stringLiteral() {
+        while (peek() != '"' && !isAtEnd()) {
+            if (peek() == '\n') {
+                line++;
+                advance();
+            }
+            if (isAtEnd()) {
+                CraftingInterpreters.error(line, "Unterminated string!");
+            }
+            advance();
+            String value = source.substring(start + 1, current - 1);
+            addToken(TokenType.STRING, value);
+        }
+    }
+
+    private void numberLiteral() {
+        while (isDigit(peek())) {
+            advance();
+        }
+        if (peek() == '.' && isDigit(peekNext())) {
+            advance();
+            while (isDigit(peek())) {
+                advance();
+            }
+        }
+        addToken(TokenType.NUMBER,
+                Double.parseDouble(source.substring(start, current)));
+    }
+
+    private boolean isDigit(char c) {
+        return c >= '0' && c <= '9';
+    }
+
+    private boolean isAlpha(char c) {
+        return (c >= 'a' && c <= 'z') ||
+                (c >= 'A' && c <= 'Z') ||
+                c == '_';
+    }
+
+    private boolean isAlphaNumeric(char c) {
+        return isAlpha(c) || isDigit(c);
+    }
+
+    private boolean isKeyword(String string) {
+        return keywords.containsKey(string);
     }
 }
